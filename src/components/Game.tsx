905680e6b2106bbb53728bef6ac57802ac323096
Hiro Funatsuka <hiro.funatsuka@gmail.com>
2025-07-20 17:53:16 +0900

    Refactor Game.tsx by changing xIsNext

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -2,21 +2,19 @@ import { useState } from "react"
 import Board from "./Board"
 
 const Game = () => {
-    const [xIsNext, setXIsNext] = useState(true)
     const [history, setHistory] = useState<string[][]>([Array(9).fill(null)])
     const [currentMove, setCurrentMove] = useState(0)
+    const xIsNext = currentMove % 2 === 0
     const currentSquares = history[currentMove]
 
     const handlePlay = (nextSquares: string[]) => {
         const nextHistory = [...history.slice(0, currentMove + 1), nextSquares]
         setHistory(nextHistory)
         setCurrentMove(nextHistory.length - 1)
-        setXIsNext(!xIsNext)
     }
 
     const jumpTo = (nextMove: number) => {
         setCurrentMove(nextMove)
-        setXIsNext(nextMove % 2 === 0)
     }
 
     const moves = history.map((squares, move) => {
