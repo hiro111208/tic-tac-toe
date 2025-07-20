cb969e0ed02119bf4eff7d1da327ab86bd6b8ab1
Hiro Funatsuka <hiro.funatsuka@gmail.com>
2025-07-20 17:13:39 +0900

    Take turns

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -2,11 +2,18 @@ import { useState } from "react";
 import Square from "./Square";
 
 const Board = () => {
+    const [xIsNext, setXIsNext] = useState(true)
     const [squares, setSquares] = useState<string[]>(Array(9).fill(null))
     const handleClick = (i: number) => {
+        if (squares[i]) return
         const nextSquares = squares.slice()
-        nextSquares[i] = 'X'
+        if (xIsNext) {
+            nextSquares[i] = 'X'
+        } else {
+            nextSquares[i] = 'O'
+        }
         setSquares(nextSquares)
+        setXIsNext(!xIsNext)
     }
     return (
         <>
