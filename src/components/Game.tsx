cc17b6e79dce810871c82a63065711be72f28f0e
Hiro Funatsuka <hiro.funatsuka@gmail.com>
2025-07-20 17:50:20 +0900

    Implement time travel

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -4,18 +4,43 @@ import Board from "./Board"
 const Game = () => {
     const [xIsNext, setXIsNext] = useState(true)
     const [history, setHistory] = useState<string[][]>([Array(9).fill(null)])
-    const currentSquares = history[history.length - 1]
+    const [currentMove, setCurrentMove] = useState(0)
+    const currentSquares = history[currentMove]
+
     const handlePlay = (nextSquares: string[]) => {
-        setHistory([...history, nextSquares])
+        const nextHistory = [...history.slice(0, currentMove + 1), nextSquares]
+        setHistory(nextHistory)
+        setCurrentMove(nextHistory.length - 1)
         setXIsNext(!xIsNext)
     }
+
+    const jumpTo = (nextMove: number) => {
+        setCurrentMove(nextMove)
+        setXIsNext(nextMove % 2 === 0)
+    }
+
+    const moves = history.map((squares, move) => {
+        let description
+        if (move > 0) {
+            description = `Go to move #${move}`
+        } else {
+            description = 'Go to game start'
+        }
+        return (
+            <li key={move}>
+                <button onClick={() => jumpTo(move)}>{description}</button>
+            </li>
+        )
+    })
+
+
     return (
         <div className="game">
             <div className="game-board">
                 <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
             </div>
             <div className="game-info">
-                <ol>{/* todo */}</ol>
+                <ol>{moves}</ol>
             </div>
         </div>
     )
